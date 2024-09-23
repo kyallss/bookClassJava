d878a79456f05769d9148f1e085196223a673033
kyallss <150505408+kyallss@users.noreply.github.com>
2024-09-23 15:02:53 +0600

    Update book.java

--- book.java
+++ book.java
@@ -35,24 +35,4 @@ public class Book {
             return this.title + " and " + otherBook.title + " have the same price.";
         }
     }
-    // Main method to test the class
-    public static void main(String[] args) {
-        Book defaultBook = new Book();
-        Book parameteriziedBook1 = new Book("Great Expectations", "Charles Dickens", "9781435171640", 45);
-        Book parameteriziedBook2 = new Book("Old Mortality", "Walter Scott", "99780199555307", 40);
-
-        System.out.println("Default Book:");
-        defaultBook.displayInfo();
-
-        System.out.println("\nParameterizied Book 1:");
-        parameteriziedBook1.displayInfo();
-
-        System.out.println("\nParameterizied Book 2:");
-        parameteriziedBook2.displayInfo();
-
-        // Comparing prices
-        System.out.println("\nPrice Comparison:");
-        System.out.println(parameteriziedBook1.comparePrice(parameteriziedBook2));
-        System.out.println(parameteriziedBook2.comparePrice(defaultBook));
-    }
-}
+  
